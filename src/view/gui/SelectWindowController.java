b44bdefb1d15a4d15487c356a146242d1a91513b
Al3ks1002 <al3ks1002@gmail.com>
2017-01-25 11:53:14 +0200

    finished

--- src/view/gui/SelectWindowController.java
+++ src/view/gui/SelectWindowController.java
@@ -326,10 +326,21 @@ public class SelectWindowController implements Initializable {
 
     ///////////
 
+    Statement
+        ex15 =
+        new CompoundStatement(new NewLockStatement("q"), new CompoundStatement(new ForkStatement(
+            new CompoundStatement(new AssignmentStatement("v", new ConstantExpression(1)),
+                new CompoundStatement(new PrintStatement(new VariableExpression("v")),
+                    new LockStatement("q")))), new LockStatement("q")));
+
+    Statement ex16 = new CompoundStatement(new NewLockStatement("x"), new UnlockStatement("q"));
+    Statement ex17 = new CompoundStatement(new NewLockStatement("x"), new LockStatement("q"));
+
     programStatements =
         new ArrayList<>(
             Arrays
-                .asList(ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9, ex10, ex11, ex12, ex13, ex14));
+                .asList(ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9, ex10, ex11, ex12, ex13, ex14,
+                    ex15, ex16, ex17));
   }
 
   private List<String> getStringRepresentations() {
